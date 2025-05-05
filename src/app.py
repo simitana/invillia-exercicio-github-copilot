26eeeef02c7157690533c76bad1566417169ece9
Luis Carlos Simitana <34255475+simitana@users.noreply.github.com>
2025-05-05 21:48:32 +0000

    Valida se o aluno já está inscrito antes de adicionar à lista de participantes

--- src/app.py
+++ src/app.py
@@ -98,9 +98,9 @@ def signup_for_activity(activity_name: str, email: str):
     # Get the specificy activity
     activity = activities[activity_name]
 
-# Adiciona o estudante à lista de participantes
+    # Validar se o aluno já está inscrito
     if email in activity["participants"]:
-        raise HTTPException(status_code=400, detail="Already signed up for this activity")
+        raise HTTPException(status_code=400, detail="Already signed up")
 
     # Add student
     activity["participants"].append(email)
